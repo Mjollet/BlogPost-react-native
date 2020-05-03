9e907c5cf71c333a6f2676580fb5fcecca87300b
MJollet <48050378+Mjollet@users.noreply.github.com>
2020-05-03 16:07:48 +0200

    with Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -11,13 +11,6 @@ const blogReducer = (state, action) => {
         case 'delete_blogpost':
             return state.filter((addBlogPost) => addBlogPost.id !== action.payload);
         
-        case 'add_blogpost':
-            return [...state, {
-                id: Math.floor(Math.random() * 99999),
-                title: action.payload.title,
-                content: action.payload.content
-            }];
-        
         case 'edit_blogpost':
             return state.map((blogPost) => {
                 return blogPost.id === action.payload.id
@@ -42,19 +35,21 @@ const addBlogPost = (dispatch) => {
     return (title, content, callback) => {
         await jsonServer.post( '/blogposts', {title, content} );
 
-        if (callback) { callback();
-        }
+        if (callback) { callback(); }
     };
 };
 
 const deleteBlogPost = dispatch => {
-    return (id) => {
+    return async id => {
+        await jsonServer.delete(`/blogposts/${id}`);
+
         dispatch({ type: 'delete_blogpost', payload: id })
     };
 }
 
 const editBlogPost = dispatch => {
-    return (id, title, content, callback) => {
+    return async (id, title, content, callback) => {
+        await jsonServer.put(`/blogposts/${id}`, { title, content })
         dispatch({ type: 'edit_blogpost', payload: { id, title, content } })
         if (callback) { callback(); }
     };
